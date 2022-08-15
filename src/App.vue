e647a9860d23af336dd60a05635d949184cf1587
Aniket Aryamane <aryamane.aniket@gmail.com>
2022-08-15 21:14:41 +0530

    Adding vue-router and creating views/pages

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 </script>
 
 <template>
+	<div>
+		<router-link :to="{ name: 'HomeView' }">Home</router-link> |
+		<router-link :to="{ name: 'AboutView' }">About</router-link>
+	</div>
+	<router-view />
 </template>
 
 <style>
